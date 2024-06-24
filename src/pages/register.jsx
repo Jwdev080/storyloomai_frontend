0f1c0175e01b3ef03012fd821e52883365c528d0
Jwdev080 <148032109+Jwdev080@users.noreply.github.com>
2024-06-24 01:02:28 +0100

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -2,8 +2,6 @@ import { Helmet } from 'react-helmet-async';
 
 import { RegisterView } from 'src/sections/register';
 
-// ----------------------------------------------------------------------
-
 export default function RegisterPage() {
   return (
     <>
